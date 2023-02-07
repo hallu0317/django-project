cb84797cad0900b55aac34a1230665a669b9f30b
kjiwon <hallu0317@gmail.com>
2023-02-07 23:52:31 +0900

    3-04 답변 개수 표시

--- project/views.py
+++ project/views.py
@@ -4,9 +4,13 @@ from django.utils import timezone
 from .models import Question, Answer
 from .forms import QuestionForm, AnswerForm
 from django.http import HttpResponseNotAllowed
+from django.core.paginator import Paginator
 def index(request):
+    page = request.GET.get('page', '1')
     question_list = Question.objects.order_by('-create_date')
-    context = {'question_list': question_list}
+    paginator = Paginator(question_list, 10)
+    page_obj = paginator.get_page(page)
+    context = {'question_list': page_obj}
     return render(request, 'project/question_list.html', context)
 
 def detail(request, question_id):

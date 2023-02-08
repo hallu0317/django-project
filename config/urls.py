40ab33350b2e5dbf75c3762c98c4cf2e66b99c5f
kjiwon <hallu0317@gmail.com>
2023-02-09 01:28:23 +0900

    3-10 views.py 분리

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from project import views
+from project.views import base_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('project/', include('project.urls')),
     path('common/', include('common.urls')),
-    path('', views.index, name='index'),  # '/' 에 해당되는 path
+    path('', base_views.index, name='index'),  # '/' 에 해당되는 path
 ]
